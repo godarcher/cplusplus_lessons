8d87a53e82999e542b45c949a085d93d914f32ac
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-01-26 12:05:58 +0100

    added initial beginnings

---
+++ encryption.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    //create an int called input and parse user input into it
+    int input;
+    cin >> input;
+
+    //TODO --> think of encryption
+
+    //print encrypted input
+    cout << input << endl;
+}
