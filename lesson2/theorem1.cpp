5191f735c9fc3d4dfee4f9fc201c5a152d74e104
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-03 10:25:06 +0100

    started lesson 2 by detailing int usage

---
+++ lesson2/theorem1.cpp
@@ -0,0 +1,31 @@
+
+//1: We need to include iostream to be able to work with files
+#include <iostream>
+
+//2: We always need to use namespace std
+using namespace std;
+
+//the int main is the so called main function, when running code it is called first.
+int main()
+{ 
+
+    //////////////////////
+    //LESSON 1: INTEGERS//
+    //////////////////////
+
+    //how to declare an empty int
+    int my_int; 
+
+    //how to declare an assigned int
+    int my_int2 = 3;
+
+    //how to manipulate the value of an int
+    my_int = 3;
+    my_int2 = my_int2 * 2;
+    my_int += 3; //3 is added to my_int
+    my_int++; //myint = myint + 1 (1 is added)
+
+    //how to calculate with multiple integers
+    int my_int3 = my_int * my_int2;
+
+}
