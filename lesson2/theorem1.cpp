de5b4b7f4f1b331c20a483e96e92b0285808d152
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-03 10:33:14 +0100

    added boolean lesson, started with using strings

--- lesson2/theorem1.cpp
+++ lesson2/theorem1.cpp
@@ -28,4 +28,32 @@ int main()
     //how to calculate with multiple integers
     int my_int3 = my_int * my_int2;
 
+    //////////////////////
+    //LESSON 2: BOOLEANS//
+    //////////////////////
+
+    //how to declare an assigned boolean
+    bool my_bool = true;
+    bool my_bool2 = false; 
+
+    //how to manipulate the value of a boolean
+    my_bool = false;
+
+    //how to assign one boolean to another one
+    my_bool2 = my_bool;
+
+    /////////////////////
+    //LESSON 3: STRINGS//
+    /////////////////////
+
+    //how to declare an assigned string
+    string my_string = "hello world"; //note that text needs to be between " ", otherwise we will be given an error
+
+    //how to overwrite string content
+    my_string = "new content";
+
+    
+
+
+
 }
