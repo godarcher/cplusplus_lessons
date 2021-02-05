cc6accdb8e9fe23dfa92a1ba53d239950f241f75
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 11:13:18 +0100

    another update

--- lesson3/dogname.cpp
+++ lesson3/dogname.cpp
@@ -5,3 +5,99 @@ For this practice project, there are 2 options
 2 --> read dog names from txt file with for loop and append to array
 
 */
+
+//1: We need to include iostream to be able to cin cout
+#include <iostream>
+
+//2: We always need to use namespace std
+using namespace std;
+
+//the int main is the so called main function, when running code it is called first.
+int main()
+{ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string dognames[100] = {};
+
+}
