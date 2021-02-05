e66524e8af9ff41aa52df546db64cfa798e63d51
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 11:04:01 +0100

    prepared theorem for today

--- lesson3/theorem3.cpp
+++ lesson3/theorem3.cpp
@@ -10,5 +10,53 @@ int main()
 
 //First lets start talking about the for loop again
 //the for loop can be used for two purposes
+for (int i = 0; i < 10; i++)
+{
+    string example = "reached";
+}
 
+
+//what does this say?
+int i = 0;
+if (i < 10)
+{
+    string example = "reached";
+}
+i++;
+
+//then 
+
+int i = 1;
+if (i < 10)
+{
+    string example = "reached";
+}
+i++;
+
+for (int i = 0; i < 10; i++)
+{
+
+}
+//i < 10 holds for, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//so the for loop will be ran 10 times, we can use i inside the for loop as a counter
+
+//now lets rewind how arrays work
+
+//arrays :)
+int grades[10000];
+int grades[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//lets say i have 500 classmates
+string classmates[500] = {};
+
+for (int i = 0; i++; i < size(classmates))
+{
+    classmates[i] = "name";
+}
+
+string dognames[20] = {};
+
+//handy tips for intendation
+//tab for to the right (well known)
+//shift tab for reverse (not well known)
 }
