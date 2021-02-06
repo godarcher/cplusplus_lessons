91421e7cb668e8abab694cb1c16ca857082f9501
godarcher <joostgrunwald2001@gmail.com>
2021-02-06 22:03:56 +0100

    done

---
+++ exercises/ex_lesson3.cpp
@@ -0,0 +1,32 @@
+//* dependencies
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /*
+       ! Your assignment for this week
+       ? 1: Install better comments plugin (then this strange comments will also make more sense)
+       ? 2: This week's assignment has a little information inside it about reverse engineering also
+       * 3: If you have finished this assignment, let me know and I will send you the answer model I wrote.
+    */
+
+    // TODO 1 Fill the below array, int hexadecimals with all hexadecimals
+    int hexadecimals[16];
+    // TODO 2 Fill the below array, int decimals, with all decimals
+    int decimals[10];
+    // TODO 3 Fill the below array, int binary, with all binary
+    int binary[2];
+
+    // TODO 4 Create 3 for loops to iterate over all these arrays and print all their elements.
+    int exit_condition = 10;
+    for (int i, i < exit_condition, i++)
+    {
+    }
+
+    //* BONUS ASSIGNMENT
+    //TODO 5 create code to convert binary to decimal, and decimal to hexadecimal
+    //TODO 6 create code to convert binary to hexadecimal directly
+}
