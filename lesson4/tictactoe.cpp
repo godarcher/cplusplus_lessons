217248963ed517ad9f761638ba1d18d3e3c43ce9
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-09 11:02:34 +0100

    prepared lesson 4

---
+++ lesson4/tictactoe.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //TODO: Make code
+    //Subdivide and conquer
+    // * 1 --> make board array
+    // * 2 --> make board print
+    // * 3 --> make check win
+    // * 4 --> make move
+}
+
+//! First we will start talking about functions
+
+// Create a function
+void myFunction()
+{
+    cout << "I just got executed!";
+}
+
+int myFunction(int x)
+{
+    return 5 + x;
+}
+
+int myFunction(int x, int y)
+{
+    return x + y;
+}
